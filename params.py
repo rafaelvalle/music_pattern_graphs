99d83ba392b451af4b3b20521d7352dac3c0bd29
Rafael Valle <jrafaelvalle@gmail.com>
2016-03-22 12:12:27 -0700

    adding re special chars

--- params.py
+++ params.py
@@ -2,3 +2,4 @@ data_folder = ('/Users/rafaelvalle/Desktop/research/'
                'music_pattern_graphs/data/midi')
 missing_key_csv = 'files_missing_key.csv'
 broken_csv = 'files_broken.csv'
+special_chrs = ('.', '^', '$', '*', '+', '?')
