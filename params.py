eccaada5a47a66c3e6852f330fb293764763bb09
Rafael Valle <jrafaelvalle@gmail.com>
2016-04-06 22:09:07 -0700

    adding other files

--- params.py
+++ params.py
@@ -2,4 +2,17 @@ data_folder = ('/Users/rafaelvalle/Desktop/research/'
                'music_pattern_graphs/data/midi')
 missing_key_csv = 'files_missing_key.csv'
 broken_csv = 'files_broken.csv'
-special_chrs = ('.', '^', '$', '*', '+', '?')
+patt_gph_ext = 'pg'
+images_folder = 'images/'
+patt_gph_img_folder = 'images/pattern_graphs/'
+patt_seq_img_folder = 'images/pattern_sequences/'
+segm_img_folder = 'images/segmentation/'
+patt_gph_folder = 'pattern_graphs/'
+unitary_pg_name = 'unitary'
+train_dic_name = 'train_dic.pkl'
+test_dic_name = 'test_dic.pkl'
+train_test_ratio = .8
+patterns = ('F', 'S', 'T')
+state_space_size = 2**(16+16) * len(patterns)
+special_chrs = ('\\', '.', '^', '$', '*', '+', '?',
+                '[', ']', '|', '(', ')', '-')
--- pattern_mining.py
+++ pattern_mining.py
@@ -1,5 +1,7 @@
 import re
+from collections import defaultdict
 import numpy as np
+from helpers import Zero
 
 
 def set_trace():
@@ -12,6 +14,41 @@ re_dict = {}
 re_dict['fol'] = lambda symbs: ur"({x}{y})".format(x=symbs[0], y=symbs[1])
 re_dict['sur'] = lambda symbs: ur"({x}{y}{x})".format(x=symbs[0], y=symbs[1])
 re_dict['unt'] = lambda symbs: ur"({x}{x}+{y})".format(x=symbs[0], y=symbs[1])
+
+
+def mineUntSurFol(seq):
+    patt_seq = []
+    patt_dic = defaultdict(Zero)
+
+    if len(seq) == 2:
+        patt_dic[(seq[0], 'F', seq[1])] += 1
+        patt_seq.append((seq[0], 'F', seq[1]))
+        return [key], patt_dic
+
+    i = 0
+    while i < len(seq) - 1:
+        if seq[i] == seq[i+1]:
+            if i < len(seq)-2:
+                if seq[i+2] != seq[i]:
+                    patt_seq.append((seq[i], 'T', seq[i+2]))
+                    patt_dic[(seq[i], 'T', seq[i+2])] += 1
+                    i += 2
+                else:
+                    i += 1
+            else:
+                patt_seq.append((seq[i], 'T', seq[i+1]))
+                patt_dic[(seq[i], 'T', seq[i+1])] += 1
+                i += 1
+        else:
+            if i < len(seq) - 2 and seq[i] == seq[i+2]:
+                patt_seq.append((seq[i], 'S', seq[i+1]))
+                patt_dic[(seq[i], 'S', seq[i+1])] += 1
+                i += 2
+            else:
+                patt_seq.append((seq[i], 'F', seq[i+1]))
+                patt_dic[(seq[i], 'F', seq[i+1])] += 1
+                i += 1
+    return (np.array(patt_seq, dtype=object), patt_dic)
 
 
 def minePatterns(patts, conns, word):
